4c776b905910ffa790fc9b91fc250e69a8b4caa0
tttuuu13 <tim.ib.222005@gmail.com>
2024-12-12 16:16:26 +0300

    Point 3

---
+++ tmibragimovPW5/Models/ArticleCellModel.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleCellModel.swift
+//  tmibragimovPW5
+//
+//  Created by тимур on 12.12.2024.
+//
+
+import Foundation
+import UIKit
+
+struct ArticleCellModel {
+    let title: String
+    let description: String
+    let link: URL
+    let image: UIImage
+}
--- tmibragimovPW5/Models/ArticleModels.swift
+++ tmibragimovPW5/Models/ArticleModels.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+struct NewsArchive: Decodable {
+    let articlesByMonth: [ArticlesByMonth]
+    enum CodingKeys: String, CodingKey {
+        case articlesByMonth = "results"
+    }
+}
+
+struct ArticlesByMonth: Decodable {
+    let articles: [Article]
+    enum CodingKeys: String, CodingKey {
+        case articles = "result"
+    }
+}
+
+struct Article: Decodable {
+    let title: String
+    let description: String
+    let date: String
+    let imageSrc: ImageSrc
+    enum CodingKeys: String, CodingKey {
+        case title
+        case description
+        case date
+        case imageSrc = "image_src"
+    }
+}
+
+struct ImageSrc: Decodable {
+    let large: String
+    let large2x: String
+}
